234fb8e7eef9f39c75034fd383ab66eb0b89f8ab
cerelise <291244924@qq.com>
2023-02-14 21:51:06 +0800

    commit in first time

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import com.example.demo.model.File;
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User check(String name);
+
+    boolean save(User user);
+
+    User checkpass(User user);
+
+    List<File> getFiles();
+
+    List<File> getFilePage(int pageNo,int pageSize);
+
+    void setFiles(File file);
+
+    List<File> getMyFiles(int id);
+
+    List<User> inline();
+
+    void layout(int id,int state);
+
+    void delFile(String id);
+
+    File saveFile(String id);
+
+    int getTotal();
+}
